7184e03991ed6068af79674b86e91737cd64751d
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-16 22:34:47 +0200

    implemented very basic camera movement

--- scripts/Camera3D.cs
+++ scripts/Camera3D.cs
@@ -1,10 +1,8 @@
 using Godot;
 using System;
 
-public partial class camera : Camera3D
+public partial class Camera3D : Godot.Camera3D
 {
-	public int i = 0;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,6 +11,8 @@ public partial class camera : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		i++;
+		var newPosition = Position;
+		newPosition.Y += 0.1f;
+		Position = newPosition;
 	}
 }
