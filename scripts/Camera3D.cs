ed90b758869d68b4110ed8aed031f85fe51c1289
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-16 23:16:46 +0200

    basic camera movement

--- scripts/Camera3D.cs
+++ scripts/Camera3D.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Camera3D : Godot.Camera3D
 {
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,8 +12,32 @@ public partial class Camera3D : Godot.Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
 		var newPosition = Position;
-		newPosition.Y += 0.1f;
+		var newRotation = Rotation;
+		if (@event is InputEventKey eventKey)
+		{
+			if (eventKey.Pressed && eventKey.Keycode == Key.Up)
+			{
+				newPosition.Y += 1.0f;
+			}
+			if (eventKey.Pressed && eventKey.Keycode == Key.Down)
+			{
+				newPosition.Y -= 1.0f;
+			}
+			if (eventKey.Pressed && eventKey.Keycode == Key.Right)
+			{
+				newRotation.Y -= 0.1f;
+			}
+			if (eventKey.Pressed && eventKey.Keycode == Key.Left)
+			{
+				newRotation.Y += 0.1f;
+			}
+		}
 		Position = newPosition;
+		Rotation = newRotation;
 	}
 }
