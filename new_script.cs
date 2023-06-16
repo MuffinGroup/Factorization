882eebad2d385dbe01aad71175e3f3750267ba36
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-16 17:47:52 +0200

    added 3d object

--- new_script.cs
+++ new_script.cs
@@ -1,10 +1,10 @@
 using Godot;
 using System;
 
-public partial class new_script : Node
-{
+public partial class new_script : Node {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
