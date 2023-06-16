cf654271e7f66c89c0615f5a80307ba145a32583
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-16 23:33:32 +0200

    I hate curly brackets on new lines

--- scripts/Camera3D.cs
+++ scripts/Camera3D.cs
@@ -1,43 +1,37 @@
 using Godot;
 using System;
 
-public partial class Camera3D : Godot.Camera3D
-{
+public partial class Camera3D : Godot.Camera3D {
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+    }
 
-	public override void _UnhandledInput(InputEvent @event)
-	{
-		var newPosition = Position;
-		var newRotation = Rotation;
-		if (@event is InputEventKey eventKey)
-		{
-			if (eventKey.Pressed && eventKey.Keycode == Key.Up)
-			{
-				newPosition.Y += 1.0f;
-			}
-			if (eventKey.Pressed && eventKey.Keycode == Key.Down)
-			{
-				newPosition.Y -= 1.0f;
-			}
-			if (eventKey.Pressed && eventKey.Keycode == Key.Right)
-			{
-				newRotation.Y -= 0.1f;
-			}
-			if (eventKey.Pressed && eventKey.Keycode == Key.Left)
-			{
-				newRotation.Y += 0.1f;
-			}
-		}
-		Position = newPosition;
-		Rotation = newRotation;
-	}
+    public override void _UnhandledInput(InputEvent @event) {
+        var newPosition = Position;
+        var newRotation = Rotation;
+        if (@event is InputEventKey eventKey) {
+            if (eventKey.Pressed && eventKey.Keycode == Key.Up) {
+                newPosition.Y += 1.0f;
+            }
+            if (eventKey.Pressed && eventKey.Keycode == Key.Down) {
+                newPosition.Y -= 1.0f;
+            }
+            if (eventKey.Pressed && eventKey.Keycode == Key.Right) {
+                newRotation.Y -= 0.1f;
+            }
+            if (eventKey.Pressed && eventKey.Keycode == Key.Left) {
+                newRotation.Y += 0.1f;
+            }
+            else {
+
+            }
+        }
+        Position = newPosition;
+        Rotation = newRotation;
+    }
 }
