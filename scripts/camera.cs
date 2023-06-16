603efdbc3fab241ab2096c4eb0f9d2498b802828
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-16 18:35:10 +0200

    lots of cleanup

--- scripts/camera.cs
+++ scripts/camera.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class camera : Camera3D
 {
+	public int i = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +13,6 @@ public partial class camera : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		i++;
 	}
 }
